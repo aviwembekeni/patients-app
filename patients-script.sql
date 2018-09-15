f24f21dbd70e1e2f93439eace9879e6b8d9aea40
Aviwe Ngqukumba <aviwe3226@gmail.com>
2018-09-15 09:24:48 +0200

    add script

--- patients-script.sql
+++ patients-script.sql
@@ -1,4 +1,4 @@
-drop table if exists users, weekdays, shifts;
+drop table if exists patients, appointments, medications;
 
 -- create table users
 -- (
@@ -38,4 +38,145 @@ CREATE TABLE medications
 	FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
 
+INSERT into patients
+	(id_no, fullname, address, illness, doctor_name, contact_no, doctor_no)
+VALUES
+	('8702127865289', 'Hayley Marshall', '22 Yorkshire rd Cape Town', 'Asthma', 'Dr Mkhize Jobela', '0878965432', '0678954321');
+
+INSERT into patients
+	(id_no, fullname, address, illness, doctor_name, contact_no, doctor_no)
+VALUES
+	('9803036537389', 'Nicole Keni', '20 Broklyn rd Cape Town', 'Bile', 'Dr Mkhize Jobela', '0618376384', '0678954321');
+
+INSERT into patients
+	(id_no, fullname, address, illness, doctor_name, contact_no, doctor_no)
+VALUES
+	('8501027836537', 'Liyema Poiana', '37 Sisulu st Cape Town', 'Cancer', 'Dr Lion Herden', '0768790234', '0789409876');
+
+INSERT into patients
+	(id_no, fullname, address, illness, doctor_name, contact_no, doctor_no)
+VALUES
+	('6501120987654', 'Jonas Zyne', '37 Jun st Cape Town', 'Piles', 'Dr Lion Herden', '0837209823', '08732120978');
+
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('X-ray scan', '2018-10-11', '1');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('dentist', '2018-10-19', '1');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('therapy', '2018-10-27', '1');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('X-ray scan', '2018-10-11', '2');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('dentist', '2018-10-19', '2');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('therapy', '2018-10-27', '2');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('X-ray scan', '2018-10-11', '3');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('dentist', '2018-10-19', '3');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('therapy', '2018-10-27', '3');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('X-ray scan', '2018-10-11', '4');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('dentist', '2018-10-19', '4');
+
+INSERT into appointments
+	(description, appointment_date, patient_id)
+VALUES
+	('therapy', '2018-10-27', '4');
+
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('Asthma/COPD', 'albuterol and ipratropium nebs prn, theophylline', '1', '2018-02-20');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('Swollen Tounge', 'corticosteroid, antihistamine, Diphenhydramine 25 mg bid', '1', '2018-03-03');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('GERD', 'famotidine', '1', '2018-06-25');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('Asthma/COPD', 'albuterol and ipratropium nebs prn, theophylline', '2', '2018-02-20');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('Swollen Tounge', 'corticosteroid, antihistamine, Diphenhydramine 25 mg bid', '2', '2018-03-03');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('GERD', 'famotidine', '2', '2018-06-25');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('Asthma/COPD', 'albuterol and ipratropium nebs prn, theophylline', '3', '2018-02-20');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('Swollen Tounge', 'corticosteroid, antihistamine, Diphenhydramine 25 mg bid', '3', '2018-03-03');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('GERD', 'famotidine', '3', '2018-06-25');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('Asthma/COPD', 'albuterol and ipratropium nebs prn, theophylline', '4', '2018-02-20');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('Swollen Tounge', 'corticosteroid, antihistamine, Diphenhydramine 25 mg bid', '4', '2018-03-03');
+
+INSERT into medications
+	(description, meds, patient_id, date_issued)
+VALUES
+	('GERD', 'famotidine', '4', '2018-06-25');
 
